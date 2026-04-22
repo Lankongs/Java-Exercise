f58062a46da7f2b7bf43f9fbd2794700f95ea959
jim <a0905121184@gmail.com>
2026-04-22 15:23:28 +0800

    Done Second Version

--- FirstClass4Team.java
+++ FirstClass4Team.java
@@ -2,5 +2,6 @@ public class FirstClass4Team {
 public static void main(String[] args) {
     System.out.println("這是用來練習GitHub協作的程式!");
     System.out.println("\"學號：\"01357106\"\n姓名：\"范曾畇憲\"");
+    System.out.println("\"學號：\"01357040\"\n姓名：\"盧彥辰\"");
 }
 }
