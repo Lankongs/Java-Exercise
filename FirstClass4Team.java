4ce57b036a67e726d0c2daae1da67eb7c801d51e
toast819 <Ad64169137@gmail.com>
2026-04-22 15:36:54 +0800

    finish

--- FirstClass4Team.java
+++ FirstClass4Team.java
@@ -1,7 +1,8 @@
 public class FirstClass4Team {
-public static void main(String[] args) {
-    System.out.println("這是用來練習GitHub協作的程式!");
-    System.out.println("\"學號：\"01357106\"\n姓名：\"范曾畇憲\"");
-    System.out.println("\"學號：\"01357040\"\n姓名：\"盧彥辰\"");
-}
+    public static void main(String[] args) {
+        System.out.println("這是用來練習GitHub協作的程式!");
+        System.out.println("\"學號：\"01357106\"\n姓名：\"范曾畇憲\"");
+        System.out.println("\"學號：\"01357040\"\n姓名：\"盧彥辰\"");
+        System.out.println("\"學號：\"01357036\"\n姓名：\"吳焌盟\"");
+    }
 }
